219d6064180f949e1603671a8ddfe2bb6ad6296d
vraj <vrajbhatt27@gmail.com>
2022-01-31 14:58:49 +0530

    Added modal for new form generation. All the python code is separated in Code dir

---
+++ mainApp/code/newForm.py
@@ -0,0 +1,8 @@
+def setNewForm(description, domains):
+    lst = domains.split('\r\n')
+    print("--------------------------------------")
+    print(description)
+    print()
+    print(lst)
+
+    return True
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .code import newForm
+
 
 def home(request):
+    if request.method == 'POST':
+        if request.POST["newForm"] == "True":
+            description = request.POST.get('description')
+            domains = request.POST.get('domains')
+            
+            res = newForm.setNewForm(description, domains)
+            return redirect('home')
+
+
     return render(request, 'mainApp/home.html')
